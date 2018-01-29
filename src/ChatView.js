538e1594209e525eb9eb0e839f5722e258344934
Hannes Hochreiner <hannes@hochreiner.net>
2018-01-29 19:52:16 +0100

    added some styling

--- src/ChatView.js
+++ src/ChatView.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+import {List, ListItem} from 'material-ui/List';
+import CommunicationChatBubble from 'material-ui/svg-icons/communication/chat-bubble';
+
 import {promisedPubSub as pps} from './utils';
 
 export default class ChatView extends Component {
@@ -10,17 +14,17 @@ export default class ChatView extends Component {
     sessionAttributes: {}
   };
 
-  constructor() {
-    super();
-    console.log(MediaRecorder.isTypeSupported('audio/webm\;codecs=opus'));
-    let that = this;
-    navigator.mediaDevices.getUserMedia({audio:true, video:false}).then(function(stream) {
-      that._mediaRecorder = new MediaRecorder(stream, {audioBitsPerSecond: 16000, mimeType: 'audio/webm\;codecs=opus'});
-      that._audioChunks = [];
-      that._mediaRecorder.ondataavailable = that.handleAudioDataAvailable.bind(that);
-      that._mediaRecorder.onstop = that.handleAudioRecorderStopped.bind(that);
-    });
-  }
+  // constructor() {
+    // super();
+    // console.log(MediaRecorder.isTypeSupported('audio/webm\;codecs=opus'));
+    // let that = this;
+    // navigator.mediaDevices.getUserMedia({audio:true, video:false}).then(function(stream) {
+    //   that._mediaRecorder = new MediaRecorder(stream, {audioBitsPerSecond: 16000, mimeType: 'audio/webm\;codecs=opus'});
+    //   that._audioChunks = [];
+    //   that._mediaRecorder.ondataavailable = that.handleAudioDataAvailable.bind(that);
+    //   that._mediaRecorder.onstop = that.handleAudioRecorderStopped.bind(that);
+    // });
+  // }
 
   componentDidMount() {
   }
@@ -65,33 +69,46 @@ export default class ChatView extends Component {
     });
   }
 
+  handleKeyPress(e) {
+    if (e.key === 'Enter') {
+      this.handleSubmit(e);
+      e.preventDefault();
+    }
+  }
+
   handleSubmit(event) {
     this.setState((prevState) => {
       let text = prevState.text;
       let history = prevState.history;
 
-      history.unshift(text);
+      history.unshift({
+        text: text,
+        source: 'me'
+      });
 
       return {
         history: history,
         text: ''
       };
     }, () => {
-      pps('system.getLexAudioResponse', {
-        request: this.state.history[0],
+      pps('system.getLexTextResponse', {
+        request: this.state.history[0].text,
         user: this.state.user,
         sessionAttributes: this.state.sessionAttributes
       }).then(res => {
         return Promise.all([
-          this._playAudio(res.audioResponse.audioStream),
+          this._playAudio(res.textResponse.audioStream),
           this._setState((prevState) => {
             let history = prevState.history;
 
-            history.unshift(res.audioResponse.message);
+            history.unshift({
+              text: res.textResponse.message,
+              source: 'lex'
+            });
 
             return {
               history: history,
-              sessionAttributes: res.audioResponse.sessionAttributes
+              sessionAttributes: res.textResponse.sessionAttributes
             };
           })
         ]);
@@ -146,15 +163,31 @@ export default class ChatView extends Component {
       <div>
         <AppBar title="ChatView" />
         <form onSubmit={this.handleSubmit.bind(this)}>
-          <input type="text" value={this.state.text} onChange={this.handleChange.bind(this)} />
+          <TextField
+            hintText='Start chatting'
+            value={this.state.text}
+            onChange={this.handleChange.bind(this)}
+            onKeyPress={this.handleKeyPress.bind(this)}
+          />
         </form>
-        <button onClick={this._startAudioRecording.bind(this)}>start</button>
-        <button onClick={this._stopAudioRecording.bind(this)}>stop</button>
-        <ul>
-          {this.state.history.map(e => {
-            return <li>{e}</li>;
+        <button style={{'display':'none'}} onClick={this._startAudioRecording.bind(this)}>start</button>
+        <button style={{'display':'none'}} onClick={this._stopAudioRecording.bind(this)}>stop</button>
+        <List style={{'width': '500px'}}>
+          {this.state.history.map((e, idx) => {
+            if (e.source === 'me') {
+              return <ListItem
+                  key={idx}
+                  primaryText={e.text}
+                  leftIcon={<CommunicationChatBubble />}
+                />;
+            }
+            return <ListItem
+                key={idx}
+                primaryText={e.text}
+                rightIcon={<CommunicationChatBubble />}
+              />;
           })}
-        </ul>
+        </List>
       </div>
     );
   }
